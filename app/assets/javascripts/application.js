3ae8a6aaf363e70a5e47898aa403aaf4b9730c18
Ryo Suzuki <ryooopan@googlemail.com>
2012-11-23 22:07:39 +0900

    add data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,82 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+var chart = new Highcharts.Chart({
+  chart: {
+    renderTo: 'container',
+    defaultSeriesType: 'spline',
+    marginRight: 10,
+    events: {
+      load: function() {
+        var series = this.series[0];
+        setInterval(function() {
+          var x = (new Date()).getTime(),
+          y = Math.random();
+          series.addPoint([x, y], true, true);
+        }, 1000);
+      }
+    }
+  },
+  title: {
+    text: 'Live random data',
+    style: {
+      margin: '10px 100px 0 0'
+    }
+  },
+  xAxis: {
+    type: 'datetime',
+    tickPixelInterval: 150
+  },
+  yAxis: {
+    title: {
+      text: 'Value'
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'}]
+  },
+  tooltip: {
+    formatter: function() {
+      return '<b>' + this.series.name + '</b><br/>' + Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' + Highcharts.numberFormat(this.y, 2);
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  exporting: {
+    enabled: false
+  },
+  plotOptions: {
+    spline: {
+      dataLabels: {
+        enabled: true,
+        formatter: function() {
+          if (!this.series.inc) this.series.inc = 1;
+          if (this.series.inc >= parseInt(this.series.data.length)) {
+            this.series.inc = 0;
+            return this.point.y;
+          }
+          this.series.inc++;
+        }
+      }
+    }
+  },
+  series: [{
+    name: 'Random data',
+    data: (function() {
+      var data = [],
+      time = (new Date()).getTime(),
+      i;
+      for (i = -19; i <= 0; i++) {
+        data.push({
+          x: time + i * 1000,
+          y: Math.random()
+        });
+      }
+      return data;
+    })()
+  }]
+});​
