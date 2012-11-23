889aabc5ebb4e6322379e641633571e4e09288f8
Ryo Suzuki <ryooopan@googlemail.com>
2012-11-24 01:10:47 +0900

    comp

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,3 +1,10 @@
 class Battery < ActiveRecord::Base
   attr_accessible :watt
+  validates :watt, :presence => true
+
+  after_create :hoge
+  def hoge
+    Battery.where(:id => self.id - 5000).destroy_all
+  end
+
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,3 +1,11 @@
 class Power < ActiveRecord::Base
   attr_accessible :watt
+
+  validates :watt, :presence => true
+
+  after_create :hoge
+  def hoge
+    Power.where(:id => self.id - 5000).destroy_all
+  end
+
 end
