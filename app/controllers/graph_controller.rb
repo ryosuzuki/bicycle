f6ac20c4f017a9d7de5a93bdff0dbbe50488b6f2
Ryo Suzuki <ryooopan@googlemail.com>
2012-11-24 00:30:16 +0900

    gheo

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -4,9 +4,11 @@ class GraphController < ApplicationController
     @power = Power.pluck(:watt)
   end
 
-  def add 
-    Power.create(:watt => params[:power])
-    Battery.create(:watt => params[:battery])
+  def add
+    if params[:power].present? && params[:battery].present?
+      Power.create(:watt => params[:power])
+      Battery.create(:watt => params[:battery])
+    end
   end
 
 end
