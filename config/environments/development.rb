e783763f5feb43bb6a71e21b1cb15398d95cbb88
Ryo Suzuki <ryooopan@googlemail.com>
2012-11-25 00:17:20 +0900

    modify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,5 @@
 Bicycle::Application.configure do
 
-  require 'pusher'
-  
-  Pusher.app_id = '32274'
-  Pusher.key = '13d2df4f6c18f0a24bfd'
-  Pusher.secret = '72d719d63f663e57c0f8'
-  
-
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
